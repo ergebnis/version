6ed58fd8836a6223f470f918fe013c8fe8d4d050
Andreas Möller <am@localheinz.com>
2023-12-26 19:16:04 +0100

    Fix: Move data providers

--- test/DataProvider/PreReleaseProvider.php
+++ test/DataProvider/PreReleaseProvider.php
@@ -16,6 +16,75 @@ namespace Ergebnis\Version\Test\DataProvider;
 final class PreReleaseProvider
 {
     /**
+     * @see https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+     * @see https://regex101.com/r/Ly7O1x/3/
+     *
+     * @return \Generator<string, array{0: string}>
+     */
+    public static function invalid(): \Generator
+    {
+        $values = [
+            '', // use named constructor to create an empty pre-release
+            '0123',
+            '0123.0123',
+            'alpha_beta',
+            'alpha.',
+            'alpha..',
+            'alpha..1',
+            'alpha...1',
+            'alpha....1',
+            'alpha.....1',
+            'alpha......1',
+            'alpha.......1',
+            '-1.0.3-gamma+b7718',
+            '+justmeta',
+            '9.8.7+meta+meta',
+            '9.8.7-whatever+meta+meta',
+            '99999999999999999999999.999999999999999999.99999999999999999----RC-SNAPSHOT.12.09.1--------------------------------..12',
+        ];
+
+        foreach ($values as $value) {
+            yield $value => [
+                $value,
+            ];
+        }
+    }
+
+    /**
+     * @see https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+     * @see https://regex101.com/r/Ly7O1x/3/
+     *
+     * @return \Generator<string, array{0: string}>
+     */
+    public static function valid(): \Generator
+    {
+        $values = [
+            'prerelease',
+            'alpha',
+            'beta',
+            'alpha.beta',
+            'alpha.beta.1',
+            'alpha.1',
+            'alpha0.valid',
+            'alpha.0valid',
+            'alpha-a.b-c-somethinglong',
+            'rc.1',
+            'DEV-SNAPSHOT',
+            'SNAPSHOT-123',
+            'alpha.1227',
+            '---RC-SNAPSHOT.12.9.1--.12',
+            '---R-S.12.9.1--.12',
+            '0A.is.legal',
+        ];
+
+        foreach ($values as $value) {
+            yield $value => [
+                $value,
+            ];
+        }
+    }
+
+    /**
      * @see https://semver.org/#spec-item-11
      * @see https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
      * @see https://regex101.com/r/Ly7O1x/3/
--- test/Unit/PreReleaseTest.php
+++ test/Unit/PreReleaseTest.php
@@ -24,7 +24,7 @@ final class PreReleaseTest extends Framework\TestCase
 {
     use Test\Util\Helper;
 
-    #[Framework\Attributes\DataProvider('provideInvalidValue')]
+    #[Framework\Attributes\DataProviderExternal(Test\DataProvider\PreReleaseProvider::class, 'invalid')]
     public function testFromStringRejectsInvalidValue(string $value): void
     {
         $this->expectException(Exception\InvalidPreRelease::class);
@@ -32,81 +32,12 @@ final class PreReleaseTest extends Framework\TestCase
         PreRelease::fromString($value);
     }
 
-    /**
-     * @see https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
-     * @see https://regex101.com/r/Ly7O1x/3/
-     *
-     * @return \Generator<string, array{0: string}>
-     */
-    public static function provideInvalidValue(): \Generator
-    {
-        $values = [
-            '', // use named constructor to create an empty pre-release
-            '0123',
-            '0123.0123',
-            'alpha_beta',
-            'alpha.',
-            'alpha..',
-            'alpha..1',
-            'alpha...1',
-            'alpha....1',
-            'alpha.....1',
-            'alpha......1',
-            'alpha.......1',
-            '-1.0.3-gamma+b7718',
-            '+justmeta',
-            '9.8.7+meta+meta',
-            '9.8.7-whatever+meta+meta',
-            '99999999999999999999999.999999999999999999.99999999999999999----RC-SNAPSHOT.12.09.1--------------------------------..12',
-        ];
-
-        foreach ($values as $value) {
-            yield $value => [
-                $value,
-            ];
-        }
-    }
-
-    #[Framework\Attributes\DataProvider('provideValidValue')]
+    #[Framework\Attributes\DataProviderExternal(Test\DataProvider\PreReleaseProvider::class, 'valid')]
     public function testFromStringReturnsPreRelease(string $value): void
     {
         $preRelease = PreRelease::fromString($value);
 
         self::assertSame($value, $preRelease->toString());
-    }
-
-    /**
-     * @see https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
-     * @see https://regex101.com/r/Ly7O1x/3/
-     *
-     * @return \Generator<string, array{0: string}>
-     */
-    public static function provideValidValue(): \Generator
-    {
-        $values = [
-            'prerelease',
-            'alpha',
-            'beta',
-            'alpha.beta',
-            'alpha.beta.1',
-            'alpha.1',
-            'alpha0.valid',
-            'alpha.0valid',
-            'alpha-a.b-c-somethinglong',
-            'rc.1',
-            'DEV-SNAPSHOT',
-            'SNAPSHOT-123',
-            'alpha.1227',
-            '---RC-SNAPSHOT.12.9.1--.12',
-            '---R-S.12.9.1--.12',
-            '0A.is.legal',
-        ];
-
-        foreach ($values as $value) {
-            yield $value => [
-                $value,
-            ];
-        }
     }
 
     public function testEmptyReturnsPreRelease(): void
