bdfbff57b1b4d24a2595d157bf507e8b0c480f8a
Andreas Möller <am@localheinz.com>
2026-04-11 17:00:30 +0200

    Fix: Run 'make refactoring'

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 use Ergebnis\License;
 use Ergebnis\PhpCsFixer;
+use PhpCsFixer\Finder;
 
 $license = License\Type\MIT::markdown(
     __DIR__ . '/LICENSE.md',
@@ -28,7 +29,7 @@ $license->save();
 
 $ruleSet = PhpCsFixer\Config\RuleSet\Php74::create()->withHeader($license->header());
 
-$finder = \PhpCsFixer\Finder::create()
+$finder = Finder::create()
     ->exclude([
         '.build/',
         '.github/',
